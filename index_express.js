d9252817c1711aa5450c0e44981b2d1b92f01ac6
Chan01116 <ktiun9630@gmail.com>
2024-11-18 16:01:52 +0900

    2024 11 18 dbconn conneting...

--- index_express.js
+++ index_express.js
@@ -2,6 +2,16 @@ const express = require('express'); //express를 임포트? 한다
 const app = express();
 const port = 3002;
 
+var dbconn = require(__dirname+"/dbconn.js");
+var conn = dbconn.init();
+dbconn.connect(conn); //에러가 나는지 안나는지 체크
+
+var bodyParser = require("body-parser"); //데이터 추출 파서 사용
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));  //글씨가 깨지지 않도록
+
+
+
 app.use("/main",express.static(__dirname+"/public")); //메인에 행당하는 경로 (디렉토리네임)     가상경로와 폴더매칭  스프링에서 리소시스를 사용했을때 처럼
 
 
@@ -11,6 +21,16 @@ app.get("/",
         res.sendFile(__dirname+"/index.html");
     }
 );
+
+app.post("/boardWriteAction",function(req,res){
+
+    var body = req.body;
+    console .log(body);
+}){
+
+}
+
+
 // app.get("/",(req.res)=>){res.sendFile(__dirname+"/index.html");
 // };
 
@@ -23,7 +43,7 @@ app.get("/",
 
 
 app.listen(port,()=>{
-    console.log("Listen : ${port}");
+    console.log("server running~~~~");
 
 });
 
